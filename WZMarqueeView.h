4c6cacebf76529827bcfab020a94c317145af805
李思迪 <lisidinetwork@gmail.com>
2017-12-15 13:04:08 +0800

    init

---
+++ WZMarqueeView.h
@@ -0,0 +1,13 @@
+//
+//  WZMarqueeView.h
+//  XYHiRepairs
+//
+//  Created by lisd on 2017/12/15.
+//  Copyright © 2017年 Kingnet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WZMarqueeView : NSObject
++ (void)hehe;
+@end
---
+++ WZMarqueeView.m
@@ -0,0 +1,15 @@
+//
+//  WZMarqueeView.m
+//  XYHiRepairs
+//
+//  Created by lisd on 2017/12/15.
+//  Copyright © 2017年 Kingnet. All rights reserved.
+//
+
+#import "WZMarqueeView.h"
+
+@implementation WZMarqueeView
++ (void)hehe {
+    NSLog(@"hehenimwei");
+}
+@end
